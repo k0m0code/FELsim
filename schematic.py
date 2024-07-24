08068eae9f88124011eb8907ef57fbac4f154065
Christian Komo <50189111+k0m0code@users.noreply.github.com>
2024-07-24 13:41:00 -0700

    Added functions and fixed wording

--- schematic.py
+++ schematic.py
@@ -1,4 +1,5 @@
-# schematic.py
+#   chematic.py
+#   Authors: Christian Komo, Niels Bidault
 
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
@@ -7,6 +8,9 @@ import numpy as np
 from beamline import *
 import datetime
 from matplotlib.widgets import Slider
+
+#Add legend for graphs 
+#Same total pipe length but different interval should end with the same standard deviation
 
 class draw_beamline:
     def __init__(self):
@@ -46,7 +50,7 @@ class draw_beamline:
             xaxisMax += intTrack
             if isinstance(beamSegments[i], driftLattice):
                 while intTrack >= interval:
-                    matrixVariables = np.array(beamSegments[i].getDriftMatrice(matrixVariables, length = interval))
+                    matrixVariables = np.array(beamSegments[i].useDriftMatrice(matrixVariables, length = interval))
                     xUpdated.append(np.std(matrixVariables[:,0]))
                     yUpdated.append(np.std(matrixVariables[:,2]))
                     xMean.append(np.mean(matrixVariables[:,0]))
@@ -54,16 +58,16 @@ class draw_beamline:
                     intTrack -= interval
                     x_axis.append(round(x_axis[-1]+interval, 3))
                 if intTrack > 0:
-                    matrixVariables = np.array(beamSegments[i].getDriftMatrice(matrixVariables, length = intTrack))
+                    matrixVariables = np.array(beamSegments[i].useDriftMatrice(matrixVariables, length = intTrack))
                     xUpdated.append(np.std(matrixVariables[:,0]))
                     yUpdated.append(np.std(matrixVariables[:,2]))
                     xMean.append(np.mean(matrixVariables[:,0]))
                     yMean.append(np.mean(matrixVariables[:,2]))
                     x_axis.append(round(x_axis[-1]+intTrack, 3))
-                    
+
             if isinstance(beamSegments[i], qpfLattice):
                 while intTrack >= interval:
-                    matrixVariables = np.array(beamSegments[i].getQPFmatrice(matrixVariables, length = interval))
+                    matrixVariables = np.array(beamSegments[i].useQPFmatrice(matrixVariables, length = interval))
                     xUpdated.append(np.std(matrixVariables[:,0]))
                     yUpdated.append(np.std(matrixVariables[:,2]))
                     xMean.append(np.mean(matrixVariables[:,0]))
@@ -71,7 +75,7 @@ class draw_beamline:
                     intTrack -= interval
                     x_axis.append(round(x_axis[-1]+interval, 3))
                 if intTrack > 0:
-                    matrixVariables = np.array(beamSegments[i].getQPFmatrice(matrixVariables, length = intTrack))
+                    matrixVariables = np.array(beamSegments[i].useQPFmatrice(matrixVariables, length = intTrack))
                     xUpdated.append(np.std(matrixVariables[:,0]))
                     yUpdated.append(np.std(matrixVariables[:,2]))
                     xMean.append(np.mean(matrixVariables[:,0]))
@@ -84,9 +88,9 @@ class draw_beamline:
             for i in range(len(x_axis)):
                 self.csvWriteData(name, x_axis[i], xUpdated[i], yUpdated[i], xMean[i], yMean[i])
 
-        print(x_axis)
-        print(xUpdated)
-        print(yUpdated)
+        # print(x_axis)
+        # print(xUpdated)
+        # print(yUpdated)
         fig, ax = plt.subplots()
         plt.plot(x_axis, xUpdated)
         plt.plot(x_axis, yUpdated)
@@ -112,22 +116,6 @@ class draw_beamline:
         
         plt.tight_layout()
         plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -160,3 +148,28 @@ class draw_beamline:
 
         plt.grid(True)
         plt.show()
+
+
+
+    #integrate getDriftMatrice within this function or get rid of getDriftMatrice altogether
+    # Can length variable be negative?
+    def driftTransformScatter(self, x_pos: list[int], y_pos: list[int], phase_x: list[int], phase_y: list[int], length, plot = True):
+        x_transform = []
+        y_transform = []
+
+        for i in range(len(x_pos)):
+            x_transform.append(x_pos[i]+length*phase_x[i])
+        for i in range(len(y_pos)):
+            y_transform.append(y_pos[i] + length*phase_y[i])
+
+        if plot:
+            fig, ax = plt.subplots()
+            ax.scatter(x_pos,y_pos, c = 'blue', s=15, alpha=0.7)
+            ax.scatter(x_transform, y_transform, c = 'green', s=15, alpha=0.7)
+            ax.set_xlabel('Position x (mm)')
+            ax.set_ylabel('Position y (mm)')
+            plt.legend(loc = 'upper right')
+            plt.tight_layout()
+            plt.show()
+
+        return x_transform, y_transform
