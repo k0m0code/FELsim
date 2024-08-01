da093ec21c4fa8cdc89059d7d38d101dd5224acf
Christian Komo <50189111+k0m0code@users.noreply.github.com>
2024-07-31 19:06:56 -0700

    redid 6d plotting for graphing, working on eclipse function + alternative 6d plotting

--- ebeam.py
+++ ebeam.py
@@ -6,7 +6,7 @@ import pandas as pd
 import sympy as sp
 from sympy.plotting import plot_implicit, PlotGrid
 import matplotlib.pyplot as plt
-
+import matplotlib.gridspec as gridspec
 
 #in plotDriftTransform, add legend and gausian distribution for x and y points
 #Replace list variables that are unchanging with tuples, more efficient for calculations
@@ -37,7 +37,9 @@ class beam:
         y_vals = np.linspace(y_min, y_max, num_pts)
         X, Y = np.meshgrid(x_vals, y_vals)
         Z = gamma * (X - xc)** 2 + 2 * alpha * (X - xc) * (Y - yc) + beta * (Y - yc) ** 2 - emittance
+
         
+
         return X, Y, Z
 
     
@@ -56,11 +58,31 @@ class beam:
             phi = 90 * np.arctan2(2 * alpha, gamma - beta) / np.pi
             tmp = pd.DataFrame([[emittance, alpha, beta, gamma, phi]], columns=label_twiss, index=[label_axes[i]])
             twiss = pd.concat([twiss, tmp])
+
+        
         return dist_avg, dist_cov, twiss
 
     def gen_6d_gaussian(self, mean, std_dev, num_particles=100):
         particles = np.random.normal(mean, std_dev, size=(num_particles, 6))
         return particles
+    
+
+
+
+
+
+
+
+
+
+    '''
+    THIS FUNCTION BELOW IS A WIP
+    '''
+
+
+
+
+
 
     # def particles_in_ellipse(self, dist_6d, n = 1):
     #     fig, axes = plt.subplots(2, 2)
@@ -72,25 +94,14 @@ class beam:
     #         X, Y, Z = self.ellipse_sym(dist_avg[2 * i], dist_avg[2 * i + 1], twiss_axis, n=n, num_pts=60)
     #         ax.scatter(dist_6d[:, 2 * i], dist_6d[:, 2 * i + 1], s=15, alpha=0.7)
     #         ax.contour(X, Y, Z, levels=[0], colors='black', linestyles='--')
-    #         # twiss_txt = '\n'.join(f'{label}: {np.round(value, 2)}' for label, value in twiss_axis.items())
-    #         # props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-    #         # ax.text(0.05, 0.95, twiss_txt, transform=ax.transAxes, fontsize=12,
-    #         #         verticalalignment='top', bbox=props)
+    #         twiss_txt = '\n'.join(f'{label}: {np.round(value, 2)}' for label, value in twiss_axis.items())
+    #         props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+    #         ax.text(0.05, 0.95, twiss_txt, transform=ax.transAxes, fontsize=12,
+    #                 verticalalignment='top', bbox=props)
 
     #     plt.tight_layout()
     #     plt.show()
     #     return
-
-
-
-
-
-
-
-
-
-
-
 
 
     def is_within_ellipse(self, x, y, xc, yc, twiss_axis, n):
@@ -117,20 +128,30 @@ class beam:
             ax = axes[i // 2, i % 2]
             twiss_axis = twiss.loc[axis]
             X, Y, Z = self.ellipse_sym(dist_avg[2 * i], dist_avg[2 * i + 1], twiss_axis, n=n, num_pts=60)
-            # print("Z1: ")
-            # print(Z)
+
             # Plot particle points and ellipse contours
             ax.scatter(dist_6d[:, 2 * i], dist_6d[:, 2 * i + 1], s=15, alpha=0.7)
             ax.contour(X, Y, Z, levels=[0], colors='black', linestyles='--')
 
             # Evaluate particles within ellipse
-            num_within_ellipse = 0          #PROGRAM RECALUCLATING Z, JUST USE OG Z VALUE 
+            num_within_ellipse = 0 
             for j in range(len(dist_6d)):
-                x, y = dist_6d[j, 2 * i], dist_6d[j, 2 * i + 1]
+                x, y = dist_6d[j, 2 * i], dist_6d[j, 2 * i + 1] #  Retrives x and y from each value list
                 # Check if the particle is within the ellipse
-                if self.is_within_ellipse(x, y, dist_avg[2 * i], dist_avg[2 * i + 1], twiss_axis, n):
+                if self.is_within_ellipse(x, y, dist_avg[2 * i], dist_avg[2 * i + 1], twiss_axis, n = n):
                     num_within_ellipse += 1
 
+            # num_within_ellipse = 0
+            # for j in Z:
+            #     for jj in j:
+            #         if jj <= 0:
+            #             num_within_ellipse += 1
+
+            twiss_txt = '\n'.join(f'{label}: {np.round(value, 2)}' for label, value in twiss_axis.items())
+            props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+            ax.text(0.05, 0.95, twiss_txt, transform=ax.transAxes, fontsize=12,
+                    verticalalignment='top', bbox=props)
+            
             count_within_ellipse.append(num_within_ellipse)
             # Optional: Display the count on the plot
             ax.set_title(f'{axis} - Phase Space\nParticles within ellipse: {num_within_ellipse}')
@@ -164,18 +185,127 @@ class beam:
 
 
 
-    def plot_6d(self, dist_6d, title):
+    # def plot_6d(self, dist_6d, title):
+
+    #     num_pts = 60  # Used for implicit plot of the ellipse
+    #     ddof = 1  # Unbiased Bessel correction for standard deviation calculation
+
+    #     dist_avg, dist_cov, twiss = self.cal_twiss(dist_6d, ddof=ddof)
+
+    #     # Define SymPy symbols for plotting
+    #     x_sym, y_sym = sp.symbols('x y')
+
+    #     fig, axes = plt.subplots(2, 2, figsize=(12, 10))
+        
+    #     x_labels = [r'Position $x$ (mm)', r'Position $y$ (mm)', r'Energy $\Delta$ $E$ (keV)', r'Position $x$ (mm)']
+    #     y_labels = [r'Phase $x^{\prime}$ (mm)', r'Phase $y^{\prime}$ (mm)', r'Time $\Delta$ $t$ (ns)',
+    #                 r'Position $y$ (mm)']
+
+    #     for i, axis in enumerate(['x', 'y', 'z']):
+
+    #         # Access Twiss parameters for the current axis
+    #         twiss_axis = twiss.loc[axis]
+
+    #         # Plot the contour where Z = 0 (the ellipse)
+    #         ax = axes[i // 2, i % 2]
+    #         ax.scatter(dist_6d[:, 2 * i], dist_6d[:, 2 * i + 1], s=15, alpha=0.7)
+    #         X, Y, Z = self.ellipse_sym(dist_avg[2 * i], dist_avg[2 * i + 1], twiss_axis, n=1, num_pts=num_pts)
+    #         ax.contour(X, Y, Z, levels=[0], colors='black', linestyles='--')
+    #         X, Y, Z = self.ellipse_sym(dist_avg[2 * i], dist_avg[2 * i + 1], twiss_axis, n=6, num_pts=num_pts)
+    #         ax.contour(X, Y, Z, levels=[0], colors='black', linestyles='--')
+
+    #         # Construct the text string from the Twiss parameters
+    #         twiss_txt = '\n'.join(f'{label}: {np.round(value, 2)}' for label, value in twiss_axis.items())
+    #         props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+    #         ax.text(0.05, 0.95, twiss_txt, transform=ax.transAxes, fontsize=12,
+    #                 verticalalignment='top', bbox=props)
+
+    #         ax.set_title(f'{axis} - Phase Space')
+    #         ax.set_xlabel(x_labels[i])
+    #         ax.set_ylabel(y_labels[i])
+    #         ax.grid(True)
+
+    #     # Plot for 'x, y - Space'
+    #     ax = axes[(i + 1) // 2, (i + 1) % 2]
+    #     ax.scatter(dist_6d[:, 0], dist_6d[:, 2], s=15, alpha=0.7)
+
+    #     ax.set_title(f'x, y - Space')
+    #     ax.set_xlabel(x_labels[i + 1])
+    #     ax.set_ylabel(y_labels[i + 1])
+    #     ax.grid(True)
+
+    #     plt.suptitle(title)
+    #     plt.tight_layout()
+    #     plt.show()
+
+    def getXYZ(self, dist_6d):
+        num_pts = 60  # Used for implicit plot of the ellipse
+        ddof = 1  # Unbiased Bessel correction for standard deviation calculation
+        dist_avg, dist_cov, twiss = self.cal_twiss(dist_6d, ddof=ddof)
+        std6 = []
+        std1 = []
+        for i, axis in enumerate(['x', 'y', 'z']):
+            # Access Twiss parameters for the current axis
+            twiss_axis = twiss.loc[axis]
+            X, Y, Z = self.ellipse_sym(dist_avg[2 * i], dist_avg[2 * i + 1], twiss_axis, n=1, num_pts=num_pts)
+            std1.append([X,Y,Z])
+            X, Y, Z = self.ellipse_sym(dist_avg[2 * i], dist_avg[2 * i + 1], twiss_axis, n=6, num_pts=num_pts)
+            std6.append([X,Y,Z])
+        return std1, std6
+    
+
+    def plotXYZ(self, dist_6d, std1, std6, ax1, ax2, ax3, ax4):
+        num_pts = 60  # Used for implicit plot of the ellipse
+        axlist = [ax1,ax2,ax3]
+        # Define SymPy symbols for plotting
+        x_sym, y_sym = sp.symbols('x y')
+        x_labels = [r'Position $x$ (mm)', r'Position $y$ (mm)', r'Energy $\Delta$ $E$ (keV)', r'Position $x$ (mm)']
+        y_labels = [r'Phase $x^{\prime}$ (mm)', r'Phase $y^{\prime}$ (mm)', r'Time $\Delta$ $t$ (ns)',
+                    r'Position $y$ (mm)']
+        dist_avg, dist_cov, twiss = self.cal_twiss(dist_6d, ddof=ddof)
+        
+        for i, axis in enumerate(['x', 'y', 'z']):
+            twiss_axis = twiss.loc[axis]
+
+            # Access Twiss parameters for the current axis
+            ax = axlist[i]
+            ddof = 1  # Unbiased Bessel correction for standard deviation calculation
+
+            ax.scatter(dist_6d[:, 2 * i], dist_6d[:, 2 * i + 1], s=15, alpha=0.7)
+            ax.contour(std1[0], std1[1], std1[2], levels=[0], colors='black', linestyles='--')
+            ax.contour(std6[0], std6[1], std6[2], levels=[0], colors='black', linestyles='--')
+
+            twiss_txt = '\n'.join(f'{label}: {np.round(value, 2)}' for label, value in twiss_axis.items())
+            props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+            ax.text(0.05, 0.95, twiss_txt, transform=ax.transAxes, fontsize=12,
+                    verticalalignment='top', bbox=props)
+
+            ax.set_title(f'{axis} - Phase Space')
+            ax.set_xlabel(x_labels[i])
+            ax.set_ylabel(y_labels[i])
+            ax.grid(True)
+
+        # Plot for 'x, y - Space'
+        ax4.scatter(dist_6d[:, 0], dist_6d[:, 2], s=15, alpha=0.7)
+
+        ax4.set_title(f'x, y - Space')
+        ax4.set_xlabel(x_labels[i + 1])
+        ax4.set_ylabel(y_labels[i + 1])
+        ax4.grid(True)
+
+
+
+    def plot_6d(self, dist_6d, ax1, ax2, ax3, ax4):
 
         num_pts = 60  # Used for implicit plot of the ellipse
         ddof = 1  # Unbiased Bessel correction for standard deviation calculation
 
         dist_avg, dist_cov, twiss = self.cal_twiss(dist_6d, ddof=ddof)
+        axlist = [ax1,ax2,ax3]
 
         # Define SymPy symbols for plotting
         x_sym, y_sym = sp.symbols('x y')
-
-        fig, axes = plt.subplots(2, 2, figsize=(12, 10))
-
+        
         x_labels = [r'Position $x$ (mm)', r'Position $y$ (mm)', r'Energy $\Delta$ $E$ (keV)', r'Position $x$ (mm)']
         y_labels = [r'Phase $x^{\prime}$ (mm)', r'Phase $y^{\prime}$ (mm)', r'Time $\Delta$ $t$ (ns)',
                     r'Position $y$ (mm)']
@@ -186,7 +316,7 @@ class beam:
             twiss_axis = twiss.loc[axis]
 
             # Plot the contour where Z = 0 (the ellipse)
-            ax = axes[i // 2, i % 2]
+            ax = axlist[i]
             ax.scatter(dist_6d[:, 2 * i], dist_6d[:, 2 * i + 1], s=15, alpha=0.7)
             X, Y, Z = self.ellipse_sym(dist_avg[2 * i], dist_avg[2 * i + 1], twiss_axis, n=1, num_pts=num_pts)
             ax.contour(X, Y, Z, levels=[0], colors='black', linestyles='--')
@@ -203,16 +333,12 @@ class beam:
             ax.set_xlabel(x_labels[i])
             ax.set_ylabel(y_labels[i])
             ax.grid(True)
+            
 
         # Plot for 'x, y - Space'
-        ax = axes[(i + 1) // 2, (i + 1) % 2]
-        ax.scatter(dist_6d[:, 0], dist_6d[:, 2], s=15, alpha=0.7)
+        ax4.scatter(dist_6d[:, 0], dist_6d[:, 2], s=15, alpha=0.7)
 
-        ax.set_title(f'x, y - Space')
-        ax.set_xlabel(x_labels[i + 1])
-        ax.set_ylabel(y_labels[i + 1])
-        ax.grid(True)
-
-        plt.suptitle(title)
-        plt.tight_layout()
-        plt.show()
+        ax4.set_title(f'x, y - Space')
+        ax4.set_xlabel(x_labels[i + 1])
+        ax4.set_ylabel(y_labels[i + 1])
+        ax4.grid(True)
