d4a9192fedff183db7f1e116fb464e2a1adff2b3
Christian Komo <50189111+k0m0code@users.noreply.github.com>
2024-09-13 16:39:46 -0700

    Update testOptimization.py

--- testOptimization.py
+++ testOptimization.py
@@ -12,15 +12,19 @@ sec3 = driftLattice(100)
 sec4 = qpdLattice(current = 32)
 line = [sec1,sec2, sec3, sec4]
 
-test = beamOptimizer(line, (0,4),50,75, [1,1], method = "COBYLA")
+test = beamOptimizer(line, (0,4),50,90, [60,40], method = "COBYLA")
 result = test.calc()
-print(test.testSpeed(10))
+# print("speed " + str(test.testSpeed(10)))
+# evals, evalType = test.testFuncEval(10)
+# print("evals " + str(evals) + ", " + evalType)
+# print("iterations " + str(test.testFuncIt(10)))
 
-# schem = draw_beamline()
-# line[1].current = result.x[0]
-# line[2].current = result.x[1]
-# schem.plotBeamPositionTransform(beam_dist, line, 10)
-# print("Current" + str(result.x))
-# print("Chi Squared:" + str(result.fun))
-# print("xstd; " + str(np.std(schem.matrixVariables[:, 0])))
-# print("ystd: " + str(np.std(schem.matrixVariables[:, 2])))
+beam_dist = test.matrixVariables
+schem = draw_beamline()
+line[1].current = result.x[0]
+line[3].current = result.x[1]
+schem.plotBeamPositionTransform(beam_dist, line, 10)
+print("Current" + str(result.x))
+print("Chi Squared:" + str(result.fun))
+print("xstd; " + str(np.std(schem.matrixVariables[:, 0])))
+print("ystd: " + str(np.std(schem.matrixVariables[:, 2])))
