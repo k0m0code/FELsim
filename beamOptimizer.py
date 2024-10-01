228b6d9431641ccb69ef5dd6fe980fa379df01bd
Christian Komo <50189111+k0m0code@users.noreply.github.com>
2024-09-30 19:16:36 -0700

    added visual iterations tracker + WIP custom objective program

--- beamOptimizer.py
+++ beamOptimizer.py
@@ -11,6 +11,7 @@ gpt"can you xplain the parameters of a constraint paramter in scipy.minimize"
 
 import scipy.optimize as spo
 from beamline import *
+from ebeam import beam
 import numpy as np
 from schematic import *
 import timeit
@@ -19,7 +20,7 @@ import matplotlib.pyplot as plt
 class beamOptimizer():
     def __init__(self, beamline, segmentVar: dict, 
                  stdxend, stdyend, method, 
-                 matrixVariables, startPoint = {}, xWeight = 1, yWeight = 1):
+                 matrixVariables, startPoint = {}, xWeight = 1, yWeight = 1, objectives = {}):
         '''
         Constructor for the optimizer object. Object is used to optimize the electric current values
         for quadruples in an accelerator beamline in order that desired particle x and y positional spread may be
@@ -59,31 +60,45 @@ class beamOptimizer():
         self.method = method
         self.xWeight = xWeight
         self.yWeight = yWeight
+        self.objectives = objectives
+
+
         self.plotChiSquared = []
         self.plotIterate = []
         self.iterationTrack = 0
 
         self.segmentVar = segmentVar
         checkSet = set()
-        self.variablesOptimize = []
+        self.variablesToOptimize = []
         for item in segmentVar:
             if (item < 0 or item >= len(beamline)):
                 raise IndexError
             varItem = segmentVar.get(item)[0]
             if varItem not in checkSet:
-                self.variablesOptimize.append(varItem)
+                self.variablesToOptimize.append(varItem)
                 checkSet.add(varItem)
 
-        self.variablesValues = [1 for i in self.variablesOptimize] 
-        self.bounds = [(None, None) for i in self.variablesOptimize]
+        self.variablesValues = [] 
+        self.bounds = []
+        self.trackVariables = []
+        for i in self.variablesToOptimize:
+            self.variablesValues.append(1) 
+            self.bounds.append((None, None))
+            self.trackVariables.append([])
+        
         for var in startPoint:
-            index = self.variablesOptimize.index(var)
+            index = self.variablesToOptimize.index(var)
             if "start" in startPoint.get(var): self.variablesValues[index] = startPoint.get(var).get("start")
             if "bounds" in startPoint.get(var): self.bounds[index] = startPoint.get(var).get("bounds")
+
+    def getXStd(particles):
+        return np.std(particles[:,0])
         
+    def getYStd(particles):
+        return np.std(particles[:,2])
         
 
-    def _optiSpeed(self, current):
+    def _optiSpeed(self, variableVals):
         '''
         Simulates particle movement through a beamline, calculates positional standard deviation, 
         and returns a chi-squared accuracy statistic. Function to call on for standard deviation
@@ -109,24 +124,28 @@ class beamOptimizer():
         for i in range(len(segments)):
             if i in self.segmentVar:
                 yFunc = self.segmentVar.get(i)[1]
-                varIndex = self.variablesOptimize.index(self.segmentVar.get(i)[0]) #  Get the index of the variable to use with 
-                objCurrent = yFunc(current[varIndex])
+                varIndex = self.variablesToOptimize.index(self.segmentVar.get(i)[0]) #  Get the index of the variable to use with 
+                objCurrent = yFunc(variableVals[varIndex])
                 param = self.segmentVar.get(i)[2]
                 particles = np.array(segments[i].useMatrice(particles, **{param: objCurrent}))
+
+                if i in self.objectives:
+                    chiPiece = 
             else:
                 particles = np.array(segments[i].useMatrice(particles))  
                 
         stdx = np.std(particles[:,0])
         stdy = np.std(particles[:,2])
+
+
         difference = np.sqrt(((stdx-self.stdxend)**2)*self.xWeight*(1/self.stdxend) + ((stdy-self.stdyend)**2)*self.yWeight*(1/self.stdyend))
         self.plotChiSquared.append(difference)
         self.plotIterate.append((self.iterationTrack) + 1)
         self.iterationTrack = self.iterationTrack + 1
+        self.trackVariables[0].append(stdx)
+        self.trackVariables[1].append(stdy)
         # print(difference)  #for testing
         return difference
-
-
-
     
     def calc(self, plot = False):
         '''
@@ -143,15 +162,29 @@ class beamOptimizer():
         result = spo.minimize(self._optiSpeed, self.variablesValues, method = self.method, bounds=self.bounds, options={'disp':True})
 
         if plot:
-            plt.plot(self.plotIterate, self.plotChiSquared)
-            plt.yscale('log')
+            fig, ax1 = plt.subplots()
+
+            chiLine, =ax1.plot(self.plotIterate, self.plotChiSquared, label = 'Chi Squared', color = 'green')
+            ax1.set_yscale('log')
+            ax1.set_ylabel('Chi-Squared Difference', color = 'green')
+            ax1.tick_params(axis='y', labelcolor = 'green')
+            ax1.spines['left'].set_color('green')
+            ax1.spines['left'].set_linewidth(1)
+
+            ax2 = ax1.twinx()
+            xStdLine, = ax2.plot(self.plotIterate, self.trackVariables[0], label = 'xstd', color = 'red')
+            yStdLine, =ax2.plot(self.plotIterate, self.trackVariables[1], label = 'ystd', color = 'blue')
+            ax2.set_ylabel('x and y standard deviation')
+            
+
+            plt.legend(handles = [chiLine, xStdLine, yStdLine], loc = 'upper right')
             plt.show()
 
         #  WIP: add results in a clean format with values of each segment
         #  Potentially add this to a seperate tinker window?
         for key in self.segmentVar:
             variable = self.segmentVar.get(key)[0]
-            index = self.variablesOptimize.index(variable)
+            index = self.variablesToOptimize.index(variable)
             yFunc = self.segmentVar.get(key)[1]
             newVal = yFunc(result.x[index])
             self.segmentVar.get(key).append(newVal)
