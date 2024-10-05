5ba8fe1db2f5ff02f684a982f62642f7b6e50c3e
Christian Komo <50189111+k0m0code@users.noreply.github.com>
2024-10-04 19:58:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,30 +3,43 @@ from pathlib import Path
 from ebeam import beam
 from schematic import draw_beamline
 from excelElements import ExcelElements
-from beamline import lattice
 
-# Create beamline from Excel file
-path2 = r"C:\Users\NielsB\cernbox\Hawaii University\Beam dynamics\FEL_sim"
-path1 = r"C:\Users\User\Documents\FELsim"
-directory = Path(path1)
-file_path = directory / 'Beamline_elements.xlsx'
-excel = ExcelElements(file_path)
-df = excel.get_dataframe()
-print(df)
-#beamlattice = lattice.changeE(E=70)
-
-beamline = excel.create_beamline()
+# # Create beamline from Excel file
+# path2 = r"C:\Users\NielsB\cernbox\Hawaii University\Beam dynamics\FEL_sim"
+# path1 = r"C:\Users\User\Documents\FELsim"
+# directory = Path(path1)
+# file_path = directory / 'Beamline_elements.xlsx'
+# excel = ExcelElements(file_path)
+# df = excel.get_dataframe()
+# print(df)
 
 
-
-#if len(beamline) >= 5:
-#    beamline = beamline[:-5]
+#beamline
+# beamline = excel.create_beamline()
+# if len(beamline) >= 5:
+#     beamline = beamline[:-5]
 schem = draw_beamline()
+
 
 
 # ebeam
 ebeam = beam()
-beam_dist = ebeam.gen_6d_gaussian(0,[1,1,1,1,0.1,450],1000)
+beam_dist = ebeam.gen_6d_gaussian(0,[1,1,1,1,1,1],1000)
+
+ebeam.plot_6d(beam_dist, 'bruh')
+
+dist_avg, dist_cov, twiss = ebeam.cal_twiss(beam_dist, ddof=1)
+print(twiss.loc['x'])
+print(twiss.loc["x"].loc[r"$\alpha$"])
+
+# schem.plotBeamPositionTransform(beam_dist, beamline, 0.2)
 
 
-schem.plotBeamPositionTransform(beam_dist, beamline, 0.4, defineLim=True, saveData=False, shape = {}, plot=True
+
+# '''
+# Shape example
+# '''
+
+# shape1 = {"shape": "circle", "radius": 5, "origin": (0,5)}
+# shape = {"shape": "rectangle", "length": 200, "width": 500, "origin": (10,-4)}
+# schem.plotBeamPositionTransform(beam_dist, beamline, 10, shape = shape)
