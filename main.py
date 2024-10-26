c94a3d482d5bd839d32e0fb69c66040ca94f45a2
Christian Komo <50189111+k0m0code@users.noreply.github.com>
2024-10-25 19:26:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,40 +3,45 @@ from pathlib import Path
 from ebeam import beam
 from schematic import draw_beamline
 from excelElements import ExcelElements
-import numpy as np
+from beamline import *
+import sys
+import time
+import pandas as pd
+from beamOptimizer import *
 
-# # Create beamline from Excel file
+pd.set_option('display.max_rows', None)
+# Create beamline from Excel file
 # path2 = r"C:\Users\NielsB\cernbox\Hawaii University\Beam dynamics\FEL_sim"
-# path1 = r"C:\Users\User\Documents\FELsim"
-# directory = Path(path1)
-# file_path = directory / 'Beamline_elements.xlsx'
-# excel = ExcelElements(file_path)
-# df = excel.get_dataframe()
-# print(df)
+path1 = r"C:\Users\User\Documents\FELsim"
+directory = Path(path1)
+file_path = directory / 'Beamline_elements(1).xlsx'
+excel = ExcelElements(file_path)
+df = excel.get_dataframe()
+print(df)
 
 
 #beamline
-# beamline = excel.create_beamline()
+beamline = excel.create_beamline()
 # if len(beamline) >= 5:
 #     beamline = beamline[:-5]
 schem = draw_beamline()
 
-print(10**(np.ceil(np.log10(0.000012342))))
+
 
 
 # ebeam
 ebeam = beam()
 beam_dist = ebeam.gen_6d_gaussian(0,[1,1,1,1,1,1],1000)
-tup = (ebeam.cal_twiss, 1)
 
+schem.plotBeamPositionTransform(beam_dist,beamline,1)
 
-
-dist_avg, dist_cov, twiss =tup[0](beam_dist, ddof=1)
-print(twiss)
-print(twiss.loc['x'])
-print(twiss.loc["x"].loc[r"$\alpha$"])
-
-# schem.plotBeamPositionTransform(beam_dist, beamline, 0.2)
+optimize = beamOptimizer(beamline, beam_dist)
+segmentVar = {1: ["I", "current", lambda num:num],
+              3: ["B", "current", lambda num:num]}
+objectives = {4: [{"measure": ["y", "alpha"],"goal":0,"weight":1},
+                  {"measure": ["x", "std"],"goal":1,"weight":1}]}
+starting = {"I": {"bounds": (0,10), "start" :5}}
+optimize.calc("COBYQA", segmentVar, starting, objectives, True, True, True)
 
 
 
