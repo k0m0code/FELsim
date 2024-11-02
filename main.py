ece9219cacdc21f6e89e561478bb4fd54909633c
Christian Komo <50189111+k0m0code@users.noreply.github.com>
2024-11-01 18:24:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import time
 import pandas as pd
 from beamOptimizer import *
 from AlgebriacOptimization import AlgebriacOpti
+import sympy.plotting as plot
 
 pd.set_option('display.max_rows', None)
 # Create beamline from Excel file
@@ -27,18 +28,15 @@ beamline = excel.create_beamline()
 schem = draw_beamline()
 
 
-seg = driftLattice(1)
-print(seg.getSymbolicMatrice())
-
-
 # ebeam
 ebeam = beam()
 beam_dist = ebeam.gen_6d_gaussian(0,[1,1,1,1,1,1],1000)
 
+'''
+create beamline, get sigma i
+'''
 alg = AlgebriacOpti()
 sig = alg.getSigmai(beam_dist)
-
-
 I = 2.28
 sec1 = driftLattice(0.5)
 sec2 = qpfLattice(current = I)
@@ -49,17 +47,49 @@ sec6 = qpfLattice(current = I)
 sec7 = driftLattice(0.25)
 sec8 = qpdLattice(current = I)
 sec9 = driftLattice(0.50)
-line = [sec1,sec2,sec4]
+line = [sec1,sec2,sec3,sec4]
 
-xvals = {1:{"current":"I1"},
-         2:{"current": "I2"}}
-ar = alg.getM(line, xvals)
-for row in ar.tolist():
+'''
+create x values to optimize
+{segment paramter: variable name}
+'''
+xvals = {
+         3: {"current":"I"},
+         1: {"current": "I"}}
+mAr = alg.getM(line, xvals)
+
+'''
+print out the M matrice of the beamline
+'''
+for row in mAr.tolist():
     print(row)
 
+'''
+print values for sigmaF, annd plot each function
+'''
+sigmaf = alg.getSigmaF(mAr,sig)
+for row in sigmaf.tolist():
+    print(str(row) + "\n")
+plot.plot(sigmaf[1])
 
+
+
+
+
+
+
+
+
+
+    
+'''
+plotting
+'''
 # schem.plotBeamPositionTransform(beam_dist,beamline,0.1, spacing = 5)
 
+'''
+optimizization example
+'''
 # optimize = beamOptimizer(beamline, beam_dist)
 # segmentVar = {1: ["I", "current", lambda num:num],
 #               3: ["B", "current", lambda num:num]}
@@ -70,10 +100,9 @@ for row in ar.tolist():
 
 
 
-# '''
-# Shape example
-# '''
-
+'''
+Shape example
+'''
 # shape1 = {"shape": "circle", "radius": 5, "origin": (0,5)}
 # shape = {"shape": "rectangle", "length": 200, "width": 500, "origin": (10,-4)}
 # schem.plotBeamPositionTransform(beam_dist, beamline, 10, shape = shape)
